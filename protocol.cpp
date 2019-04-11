eb94061a9216896ce6d0af5281dbbaa9baf044af
LongShao007 <007longshao@gmail.com>
2019-04-11 21:00:21 +0800

    change default port and message start string

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0x4c, 0x52, 0x43, 0x4e };
 
 CMessageHeader::CMessageHeader()
 {
